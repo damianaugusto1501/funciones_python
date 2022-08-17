de8e3aa8205d999c5b6786f4402bcdc828321784
Damian Augusto <60717309+damianaugusto1501@users.noreply.github.com>
2022-08-17 11:29:01 -0300

    Update ejercicio_5.py

--- ejercicios_practica/ejercicio_5.py
+++ ejercicios_practica/ejercicio_5.py
@@ -12,12 +12,33 @@
 # --------------------------------
 # Aquí copiar la función "generar_invitados"
 # ya elaborada
+def generar_invitados():
+    print("Cuantos invitados seran?")
+    cantidad_de_invitados = int(input())
+    lista_invitados = []
+
+    for invitado in range(cantidad_de_invitados):
+        print("ingrese el nombre")
+        nombre =str(input())
+        lista_invitados.append(nombre)
+        
+
+    return lista_invitados
 
 # --------------------------------
 
 # --------------------------------
 # Aquí copiar la función "ordenar"
 # ya elaborada
+
+def ordenar(numeros):
+    print("funcion ordenar")
+    nueva_lista = []
+    
+    for numero in numeros :
+        nueva_lista = sorted(numeros)
+
+    return nueva_lista
 
 # --------------------------------
 
@@ -33,16 +54,19 @@ if __name__ == '__main__':
     # Luego de copiar las funciones, invocarla en este lugar:
 
     # 1) Primero generar una lista de invitados con "generar_invitados"
-    #    Almacenar el resultado en "lista_invitados"
-
+    #    Almacenar el resultado en "lista_invitados"  
     # lista_invitados = generar_invitados()
-
+    
+    lista_de_invitados = generar_invitados()
+    
     # 2) Luego ordenar la lista de invitados con "ordenar"
     #    --> Pasar como parámetro la "lista_invitados"
     #    --> Retornar la lista de invitados ordenada
 
     # lista_invidatos_ordenada = ordenar(lista_invitados)
+    lista_invitados_ordenada = ordenar(lista_de_invitados)
+
 
     # Imprimir en pantalla "lista_invidatos_ordenada":
-
+    print("La lista ordenada seria :",lista_invitados_ordenada)
     print("terminamos")
